c305cb76eb77440b5b63ed4d85ff70ba5689add5
ritik6559 <ritikjoshi741@gmail.com>
2025-03-17 14:59:02 +0530

    server setup

---
+++ backend/index.js
@@ -0,0 +1,33 @@
+import express from "express";
+import dotenv from "dotenv";
+import cors from "cors";
+import cookieParser from "cookie-parser";
+import mongoose from "mongoose";
+
+dotenv.config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+const databaseURL = process.env.DATABASE_URL;
+
+app.use(cors({
+    origin: [ process.env.ORIGIN ],
+    method: ["GET", "POST", "PUT", "PATCH", "DELETE"],
+    credentials: true
+}));
+
+app.use(cookieParser());
+
+app.use(express.json());
+
+const server = app.listen(PORT, () => {
+    console.log(`Server started on port ${PORT}`);
+});
+
+mongoose.connect(databaseURL)
+    .then(() => {
+        console.log("MongoDB Connected");
+    })
+    .catch((err) => {
+        console.log(err);
+    })
--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,11 +1,21 @@
+import {Button} from "@/components/ui/button.jsx";
+import {BrowserRouter, Navigate, Route, Routes} from "react-router-dom";
+import Auth from "@/pages/auth/Auth.jsx";
+import Chat from "@/pages/chat/Chat.jsx";
+import Profile from "@/pages/profile/Profile.jsx";
+
 function App() {
     return (
-        <>
-            <div className="font-bold" >
-                Hello World!
-            </div>
-        </>
+
+        <BrowserRouter>
+            <Routes>
+                <Route path="/auth" element={<Auth />}  />
+                <Route path="*" element={ <Navigate to="/auth" />}  />
+                <Route path="/chat" element={<Chat />}  />
+                <Route path="/profile" element={<Profile />}  />
+            </Routes>
+        </BrowserRouter>
     )
 }
 
-export default App
+export default App;
